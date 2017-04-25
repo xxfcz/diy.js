bd8416a65b5410305ad4b229532ca47ecddf7aa9
xxfcz <xxf_cz@163.com>
2017-04-25 22:02:52 +0800

    +) $.curry() +) pure empty object: $._

--- v0.3/lib/lang_ext.js
+++ v0.3/lib/lang_ext.js
@@ -8,6 +8,64 @@ $.define('lang_ext', ['lang_fix.js'], function () {
         }
     })(Function.prototype.bind);
 
+
+    /**
+     * 函数柯里化
+     * @param fn
+     * @returns {*}
+     * 用法：
+
+    function sum(a,b,c) {
+        return a+b+c;
+    }
+
+    var r = curry(sum)(1)(2,3);
+    console.log(r); // 6
+
+     */
+    $.curry = function(fn) {
+        function inner(len, args) {
+            if (len === 0) {
+                return fn.apply(null, args);
+            }
+            return function () {
+                return inner(len - arguments.length, args.concat([].slice.apply(arguments)));
+            }
+        }
+
+        return inner(fn.length, []);
+    };
+
+
+    $.pureEmptyObject = Object.create(null);
+
+    function getPureEmptyObjectIE() {
+        var doc = new ActiveXObject('htmlfile');
+        doc.write('<script><\/script>');
+        doc.close();
+        var obj = doc.parentWindow.Object;
+        if (!obj || obj === Object) {
+            return;
+        }
+
+        var name, names = ['constructor', 'hasOwnProperty', 'isPrototypeOf',
+            'propertyIsEnumerable', 'toLocaleString', 'toString', 'valueOf'];
+        while (name = names.pop())
+            delete obj.prototype[name];
+        return obj;
+    }
+
+
+    $._ = undefined;
+
+    try {
+        $._ = Object.create(null);
+    }
+    catch (ex) {
+        $._ = getPureEmptyObjectIE();
+    }
+
+
     $.equal = function (a, b) {
         var type = $.type(a);
         if (type !== $.type(b))
